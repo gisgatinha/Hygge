7fdd4e59380259a549f94d17f619c18d40e8a839
Gis@ <60092037+gisgatinha@users.noreply.github.com>
2021-10-31 18:40:41 +0100

    Update logged.js

--- js/logged.js
+++ js/logged.js
@@ -5,18 +5,3 @@ window.addEventListener('load', () => {
 
     document.getElementById('navn').innerHTML = name;
 })
-
-window.addEventListener('load', () => {
-  
-    const name = localStorage.getItem('BRUKERNAVN');
-    const organization = localStorage.getItem('CONTACT');
-    const contact = localStorage.getItem('ORGANIZATION');
-    const email = localStorage.getItem('E-MAIL');
-
-    document.getElementById('navn').innerHTML = name;
-    document.getElementById('navnet').innerHTML = organization;
-    document.getElementById('kontaktnavn').innerHTML = contact;
-    document.getElementById('email').innerHTML = email;    
-    
-
-})
