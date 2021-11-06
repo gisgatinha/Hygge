bdc4ea7fad08307e1b70460617f1426726a9f236
Gis@ <60092037+gisgatinha@users.noreply.github.com>
2021-11-06 21:58:52 +0100

    Delete logged.js

--- js/logged.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-  
-    window.open("../calendar_logged.html");
-    const name = localStorage.getItem('NAME');
-
-    document.getElementById('navn').innerHTML = name;
-});
